acc9d8aafba857c91a585bd49f15a17b985df312
ClintonYounge <clintonyounge18@gmail.com>
2023-08-02 12:29:01 -0400

    Update person class to Nameable
    - Person class inherits from Nameable
    - Person class has a method correct_name implemented. It returns the name attribute.

--- person.rb
+++ person.rb
@@ -1,12 +1,19 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :parent_permission
 
   def initialize(name = 'Unknown', age = nil, parent_permission: true)
+    super()
     @id = rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
