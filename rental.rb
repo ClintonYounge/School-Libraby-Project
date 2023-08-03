e5c53d5a34431383dbacb0924283075505066f38
ClintonYounge <clintonyounge18@gmail.com>
2023-08-03 02:40:37 -0400

    Add rental class

---
+++ rental.rb
@@ -0,0 +1,15 @@
+require_relative 'book'
+
+class Rental
+    attr_accessor :date
+    attr_reader :book, :person
+
+    def initialize(date, book, person)
+        @date = date
+        @book = book
+        @person = person
+
+        book.add_rental(self)
+        person.add_rental(self)
+    end
+end
