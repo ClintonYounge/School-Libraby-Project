1102ba096735947754a615dcb58aa44a9878d311
ClintonYounge <clintonyounge18@gmail.com>
2023-08-05 02:04:59 -0400

    Implement create and list rental by id

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require_relative 'teacher'
 require_relative 'students'
 require_relative 'teachers'
 require_relative 'rental'
+require_relative 'menu_handler'
 
 class App
   attr_accessor :library, :students, :teachers
@@ -18,15 +19,15 @@ class App
   end
 
   def welcome
-    display_welcome_message
+    puts 'Welcome to the School\'s Library'
+    puts '--------------------------------'
   end
 
   def run
-    display_options
-    handle_user_choice
+    menu_handler = MenuHandler.new(self)
+    menu_handler.display_options
+    menu_handler.handle_user_choice
   end
-
-  private
 
   def list_all_books
     puts 'Listing all books'
@@ -133,17 +134,58 @@ class App
     run
   end
 
+  def grab_book_num
+    books = @library.grab_all_books
+
+    if books.empty?
+      puts 'There are no books in the library, press any key to continue.'
+    else
+      puts "Choose a book by number (from 0 to #{books.size - 1}):"
+      books.each_with_index do |book, index|
+        puts "#{index}) Title: '#{book.title}', Author: #{book.author}"
+      end
+    end
+    gets.chomp.to_i
+  end
+
+  def grab_person_num
+    if @people.empty?
+      puts 'There is no one in the library.'
+    else
+      puts ' '
+      puts 'Select a person from the following list by number (not id):'
+
+      @people.each_with_index do |person, index|
+        puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+    end
+    gets.chomp.to_i
+  end
+
   def create_rental
+    book_num = grab_book_num
+    person_num = grab_person_num
+
+    puts 'Please enter the date in YYYY-MM-DD format:'
+    date = gets.chomp
+
+    @rentals.push(Rental.new(date, @library.grab_all_books[book_num], @people[person_num]))
     puts 'Rental created successfully!'
     run
   end
 
   def list_rentals_for_person
     puts 'Listing all rentals for a given person id'
+    puts 'Please enter the person id:'
+    person_id = gets.chomp.to_i
+    puts ' '
+    @rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: #{rental.book.title}" if rental.person.id == person_id
+    end
     run
   end
 
   def exit
-    puts 'Exiting'
+    puts 'Thank you for using this app!'
   end
 end
--- main.rb
+++ main.rb
@@ -1,45 +1,8 @@
 require_relative 'app'
 
-class Main
-  def display_welcome_message
-    puts 'Welcome to the School\'s Library'
-    puts '--------------------------------'
-  end
-
-  def display_options
-    puts ' '
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-    puts '--------------------------------'
-  end
-
-  def handle_user_choice
-    option = gets.chomp.to_i
-
-    menu_options = {
-      1 => :list_all_books,
-      2 => :list_all_people,
-      3 => :create_person,
-      4 => :create_book,
-      5 => :create_rental,
-      6 => :list_rentals_for_person,
-      7 => :exit
-    }
-
-    if menu_options.key?(option)
-      send(menu_options[option])
-    else
-      puts 'Invalid option'
-    end
-  end
-
+def main
   app = App.new
+  app.welcome
   app.run
 end
 
