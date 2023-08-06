48c5b900f2ddae4def770199ebcc4ff5391365ce
ClintonYounge <clintonyounge18@gmail.com>
2023-08-05 20:01:17 -0400

    Introduce bug

--- student.rb
+++ student.rb
@@ -4,8 +4,8 @@ class Student < Person
   attr_accessor :classroom
   attr_reader :id
 
-  def initialize(classroom, name = 'Unknown', age = nil, parent_permission: true)
-    super(name, age, parent_permission: parent_permission)
+  def initialize(classroom, name = 'Unknown', age = nil, parent_permission)
+    super(name, age, parent_permission)
     @classroom = classroom
   end
 
