9c340b94044901fdcff62a2c1d91f058b361b148
ClintonYounge <clintonyounge18@gmail.com>
2023-08-11 10:53:41 -0400

    Run linters

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../nameable'
 
 describe Nameable do
-    before :each do
-        @nameable = Nameable.new
-    end
+  before :each do
+    @nameable = Nameable.new
+  end
 
   describe '#correct_name' do
-    it 'raises NotImplementedError' do     
-      expect {@nameable.correct_name }.to raise_error(NotImplementedError)
+    it 'raises NotImplementedError' do
+      expect { @nameable.correct_name }.to raise_error(NotImplementedError)
     end
   end
 end
