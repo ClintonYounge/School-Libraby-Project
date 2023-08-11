fcea653fa84a300931f5a81b7bcd7e800a623798
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-11 16:59:08 +0300

    Add test for nameable class

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'nameable'
+
+describe Nameable do
+    before :each do
+        @nameable = Nameable.new
+    end
+
+  describe '#correct_name' do
+    it 'raises NotImplementedError' do     
+      expect {@nameable.correct_name }.to raise_error(NotImplementedError)
+    end
+  end
+end
