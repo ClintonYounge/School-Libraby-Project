dae543d4f004a3afa6934e2dc5effa9c5e91bc1e
ClintonYounge <clintonyounge18@gmail.com>
2023-08-11 03:06:24 -0400

    Create student and teacher test using rspec

---
+++ spec/student_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../classroom'
+require_relative '../student'
+
+describe Student do
+    before :each do
+        @classroom = Classroom.new('Class A')
+        @student = Student.new(@classroom, 'Clinton Younge', 18)
+    end
+    
+    it 'validate student classroom' do
+        expect(@student.classroom).to eq(@classroom)
+    end
+
+    it 'validate the student name' do
+        expect(@student.name).to eq('Clinton Younge')
+    end
+
+    it 'validate the student age' do
+        expect(@student.age).to eq(18)
+    end
+
+    it 'validate empty list of rentals' do
+        expect(@student.rentals).to eq([])
+    end
+end
