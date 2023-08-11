a9f459b9cfb41c217f969432db81be6a8cc8959d
ClintonYounge <clintonyounge18@gmail.com>
2023-08-11 14:45:43 -0400

    Run linter

--- spec/capitalize_spec.rb
+++ spec/capitalize_spec.rb
@@ -2,16 +2,16 @@ require_relative '../capitalize_decorator'
 require_relative '../person'
 
 describe CapitalizeDecorator do
-    before :each do
-        @person = Person.new('maximilianus', 22)
-    end
+  before :each do
+    @person = Person.new('maximilianus', 22)
+  end
 
-    it 'should validate the person name' do
-        expect(@person.correct_name).to eq('maximilianus')
-    end
-    
-    it 'should capitalize the name' do
-        capitalized_name = CapitalizeDecorator.new(@person)
-        expect(capitalized_name.correct_name).to eq('Maximilianus')
-    end
+  it 'should validate the person name' do
+    expect(@person.correct_name).to eq('maximilianus')
+  end
+
+  it 'should capitalize the name' do
+    capitalized_name = CapitalizeDecorator.new(@person)
+    expect(capitalized_name.correct_name).to eq('Maximilianus')
+  end
 end
