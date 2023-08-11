32d74a7fdde3fe2ce495f2f2fc1af27ad525f896
ClintonYounge <clintonyounge18@gmail.com>
2023-08-11 03:05:30 -0400

    Create book test using rspec

---
+++ spec/book_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../book'
+
+describe Book do
+    before :each do
+        @book = Book.new('Title', 'Author')
+    end
+
+    it 'should have a title' do
+        expect(@book.title).to eq('Title')
+    end
+
+    it 'should have an author' do
+        expect(@book.author).to eq('Author')
+    end
+end
