5e1e57c89de6c6d7312f8356b7574008554f8351
ClintonYounge <clintonyounge18@gmail.com>
2023-08-11 14:45:22 -0400

    Add capitalize and trimmer tests

---
+++ spec/trimmer_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../trimmer_decorator'
+require_relative '../person'
+
+describe TrimmerDecorator do
+    before :each do
+        @person = Person.new('maximilianus', 22)
+    end
+
+    it 'should validate the person name' do
+        expect(@person.correct_name).to eq('maximilianus')
+    end
+    
+    it 'should trim the name' do
+        trimmed_person = TrimmerDecorator.new(@person)
+        expect(trimmed_person.correct_name).to eq('maximilian')
+    end
+end
